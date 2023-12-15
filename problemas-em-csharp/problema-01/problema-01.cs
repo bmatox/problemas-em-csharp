952079428d1f9ecb0f9648225eea85b0531aa22b
bmatox <araujobrunomatos@gmail.com>
2023-12-15 02:45:52 -0300

    Atualizações

---
+++ problemas-em-csharp/problema-01/problema-01.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Globalization;
+// URI 1008
+
+namespace problemas_em_csharp {
+
+    internal class problema_01 {
+        private static void Main(string[] args) {
+
+            int num, horastrab;
+            double valorh, salario;
+
+            num = int.Parse(Console.ReadLine());
+            horastrab = int.Parse(Console.ReadLine());
+            valorh = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horastrab * valorh;
+
+            Console.WriteLine("NUMBER = " +  num);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ problemas-em-csharp/problema-02/problema-02.cs
@@ -0,0 +1,14 @@
+﻿
+// URI 
+
+namespace problemas_em_csharp.problema_02 { 
+    internal class Class1 {
+        private static void Main(string[] args) {
+
+            Console.WriteLine("HELLO WORLD");
+
+        }
+
+    }
+    
+}
