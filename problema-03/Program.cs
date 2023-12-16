ebc8ef348eaac021c64cfa0c90615715bcc1150e
bmatox <araujobrunomatos@gmail.com>
2023-12-16 02:03:26 -0300

    updates

---
+++ problema-03/Program.cs
@@ -0,0 +1,38 @@
+﻿// Fazer um programa para ler as duas notas que um aluno obteve no primeiro e segundo semestres de uma disciplina anual.
+// Em seguida, mostrar a nota final que o aluno obteve no ano juntamente com um texto explicativo.
+// Caso a nota final do aluno seja inferior a 60.00, mostrar a mensagem "REPROVADO", conforme exemplos.
+// Todos os valores devem ter uma casa decimal.
+
+using System.Globalization;
+
+internal class Program {
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            double n1, n2, notafinal;
+
+            Console.WriteLine("Digite a primeira nota: ");
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nDigite a segunda nota: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            notafinal = (n1 + n2) / 2;
+
+            if (notafinal < 6)
+            {
+                Console.WriteLine("\nNOTA FINAL: " + notafinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
+            else
+            {
+                Console.WriteLine("\nNOTA FINAL: " + notafinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("APROVADO");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
