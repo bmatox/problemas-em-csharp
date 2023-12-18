69d67d0a979e401071f80473ed142174f307ac30
bmatox <araujobrunomatos@gmail.com>
2023-12-18 15:54:55 -0300

    Mais treino

---
+++ programa-06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int idade, num, soma;
+        double media;
+        soma = 0;
+        num = 0;
+        media = 0.0;
+
+        idade = int.Parse(Console.ReadLine());
+        if (idade < 0) {
+            Console.WriteLine("Impossível calcular!");
+        
+        }
+        while (idade >= 0)
+        {
+            soma += idade;
+            num += 1;
+            idade = int.Parse(Console.ReadLine());
+
+        }
+        
+        media =  (soma / (double) num);
+
+        Console.WriteLine("Média = " + media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
