204276c3905bd50271bb3d6d60c2c17315bd81e2
bmatox <araujobrunomatos@gmail.com>
2023-12-18 19:01:51 -0300

    Mais treino.

--- problema-08/Program.cs
+++ problema-08/Program.cs
@@ -3,6 +3,27 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int x, y, soma;
+        soma = 0;
+
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        if (x > y)
+        {
+            int aux = x;
+            x = y;
+            y = aux;
+        }
+
+        for (int i = (x + 1); i < y; i++)
+        {
+            if ((i % 2) != 0)
+            {
+                soma += i;
+            }
+
+        }
+        Console.WriteLine(soma);
     }
 }
---
+++ problema-09/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        n = int.Parse(Console.ReadLine());
+        int[] vet = new int[n];
+      
+        for (int i = 0; i < n; i++)
+        {
+            vet[i] = int.Parse(Console.ReadLine());
+
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(vet[i]);
+        }
+
+    }
+}
